9954f66e5a89761290e63eed5cbe3e15adb7f418
ggithubb4 <95412907+ggithubb4@users.noreply.github.com>
2022-06-09 20:41:28 +0530

    Update forcesubs.py

--- aries/modules/forcesubs.py
+++ aries/modules/forcesubs.py
@@ -179,7 +179,7 @@ def config(client, message):
 
 __help__ = """
 🔘 *Force Subscribe:*
-⇝ Aries can mute members who are not subscribed your channel until they subscribe
+⇝ I can mute members who are not subscribed your channel until they subscribe
 ⇝ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
 🔘 *Setup*
