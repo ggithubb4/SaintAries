6c8ffd5660e3e3a33262fb7ab59172f5247c014d
ggithubb4 <95412907+ggithubb4@users.noreply.github.com>
2022-06-09 14:11:28 +0530

    Update telegraph.py

--- aries/modules/telegraph.py
+++ aries/modules/telegraph.py
@@ -90,7 +90,7 @@ file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
-__mod_name__ = "Telegraph"
+__mod_name__ = "🔘 Telegraph"
 
 __help__ = """
  ❍ /tgm : Get Telegraph Link Of Replied Media
