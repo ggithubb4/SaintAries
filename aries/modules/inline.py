ead1e0caac6817bcf7a52c867a5a85fe04ac4fcb
ggithubb4 <95412907+ggithubb4@users.noreply.github.com>
2022-06-09 14:12:04 +0530

    Update inline.py

--- aries/modules/inline.py
+++ aries/modules/inline.py
@@ -3,7 +3,7 @@ import traceback
 from aries import pbot as app
 from aries.utils.inlinefuncs import *
 
-__mod_name__ = "Inline"
+__mod_name__ = "🔘 Inline"
 __help__ = """See inline for help related to inline"""
 
 
