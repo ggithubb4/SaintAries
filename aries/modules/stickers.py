4d53b8a0e72a6e429fbc5bd1cc3d428caf7494a5
ggithubb4 <95412907+ggithubb4@users.noreply.github.com>
2022-06-09 14:10:38 +0530

    Update stickers.py

--- aries/modules/stickers.py
+++ aries/modules/stickers.py
@@ -678,7 +678,7 @@ __help__ = """
   ❍ `/stickers` : Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "Stickers"
+__mod_name__ = "🔘 Stickers"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid, run_async=True)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker, run_async=True)
 KANG_HANDLER = DisableAbleCommandHandler(
