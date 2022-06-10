36f45568f4c5dbeb3621b193a3bbcf34f9f17cbb
ggithubb4 <95412907+ggithubb4@users.noreply.github.com>
2022-06-10 13:18:36 +0530

    Update aries.py

--- aries/modules/aries.py
+++ aries/modules/aries.py
@@ -130,7 +130,7 @@ dispatcher.add_handler(LAWAK_HANDLER)
 DIARYARYZA_HANDLER = DisableAbleCommandHandler("diaryaryza", diaryaryza, run_async=True)
 dispatcher.add_handler(DIARYARYZA_HANDLER)
 
-__mod_name__ = "🔘 Extras"
+__mod_name__ = "🔘 Extra"
 
 
 __command_list__ = ["aries", "diaryaryza", "lawak"]
